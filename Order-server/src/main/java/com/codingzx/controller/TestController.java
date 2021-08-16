5f14cf02820a7e3adbff2c6f245eb47170a844bf
codingxin <994683607@qq.com>
2021-08-16 21:16:55 +0800

    提交本地代码

--- Order-server/src/main/java/com/codingzx/controller/TestController.java
+++ Order-server/src/main/java/com/codingzx/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.codingzx.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author codingzx
@@ -14,7 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
+    @GetMapping("/order")
+    public String getStock() {
+        List<ServiceInstance> list = discoveryClient.getInstances("stock-server");
+        String a="";
+        for(ServiceInstance serviceInstance:list){
+            a=a+" "+serviceInstance.getHost();
+        }
+        return a;
+    }
 
 
     @GetMapping("/hello")
